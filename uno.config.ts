ea376ddfcf7a6a016f64c71894c017c2cf5ce7a8
HowieHz <howie_hzgo@outlook.com>
2024-06-12 22:57:21 +0800

    chore: unocss 添加配置，去除 preflight

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from "unocss";
-import presetIcons from "@unocss/preset-icons";
+import { defineConfig, presetMini, presetIcons  } from "unocss";
 
 // mdi cib ic ph ri typcn "ant-design" "fa-solid" "heroicons-outline" "material-symbols" "simple-icons"
 
@@ -7,7 +6,11 @@ export default defineConfig({
   content: {
     filesystem: ["./src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}", "./templates/**/*.html"]
   },
-  presets: [presetIcons({})],
+  presets: [
+    presetMini({
+      preflight: false
+    }),
+    presetIcons({})],
   cli: {
     entry: {
       patterns: ["./src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}", "./templates/**/*.html", "./templates/*.html"],
