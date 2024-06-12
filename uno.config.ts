9c4f79f9437926d8c48fb80a06322ef1392266d6
HowieHz <howie_hzgo@outlook.com>
2024-06-12 09:54:27 +0800

    chore: 引入 unocss-cli

--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig, presetIcons } from "unocss";
+import { defineConfig } from "unocss";
+import presetIcons from "@unocss/preset-icons";
 
 // mdi cib ic ph ri typcn "ant-design" "fa-solid" "heroicons-outline" "material-symbols" "simple-icons"
 
 export default defineConfig({
   content: {
-    filesystem: [
-      './src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}',
-      './templates/**/*.html',
-      './templates/*.html',
-    ],
+    filesystem: ["/src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}", "/templates/**/*.html", "/templates/*.html"],
+    pipeline: {
+      include: ["/src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}", "/templates/**/*.html", "/templates/*.html"],
+    },
   },
-  presets: [
-    presetIcons({
-    }),
-  ],
+  presets: [presetIcons({})],
+  cli: {
+    entry: {
+      patterns: ["./src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}", "./templates/**/*.html", "./templates/*.html"],
+      outFile: './tmp/styles/uno.css'
+    }
+  },
+  safelist: [
+    "i-heroicons-outline:rss", "i-heroicons-outline", "heroicons-outline", "i-heroicons-outline-rss", "heroicons-outline-rss", "heroicons-outline:rss"
+  ]
 });
