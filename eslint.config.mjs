102d56936be522931103cd93ea29d2c32af3ff23
HowieHz <howie_hzgo@outlook.com>
2025-01-25 20:27:56 +0800

    chore: 更新依赖

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,20 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import { includeIgnoreFile } from "@eslint/compat";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
   includeIgnoreFile(gitignorePath),
-  ...compat
-    .extends(
-      "eslint:recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-    )
-    .map((config) => ({
-      ...config,
-      files: ["**/*.js", "**/*.cjs", "**/*.mjs", "**/*.cts", "**/*.ts", "**/*.mts"],
-    })),
+  ...tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
   {
     files: ["**/*.js", "**/*.cjs", "**/*.mjs", "**/*.cts", "**/*.ts", "**/*.mts"],
-
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-      prettier,
-    },
 
     languageOptions: {
       globals: {
@@ -44,8 +22,7 @@ export default [
         ...globals.node,
         ...globals.jquery,
       },
-
-      parser: tsParser,
     },
   },
+  eslintPluginPrettierRecommended,
 ];
