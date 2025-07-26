e2a533e7d7b18fb768a612ad5a816985767472ac
HowieHz <howie_hzgo@outlook.com>
2025-07-26 19:50:44 +0800

    fix: lint error

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
-import { type Config } from "prettier";
-
-const config: Config = {
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
   printWidth: 120,
   tabWidth: 2,
   useTabs: false,
