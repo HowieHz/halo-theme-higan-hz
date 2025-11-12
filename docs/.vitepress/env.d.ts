b2e23d9f5a7a93d4fe007dc44e002477c51e3080
HowieHz <howie_hzgo@outlook.com>
2025-11-13 01:28:44 +0800

    chore: Remove custom Vue module declaration

--- docs/.vitepress/env.d.ts
+++ docs/.vitepress/env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="@whyframe/core/global" />
-
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<Record<string, never>, Record<string, never>, unknown>;
-  export default component;
-}
