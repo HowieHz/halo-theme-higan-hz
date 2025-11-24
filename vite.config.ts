9f8bd77f9ee552781c0a072b334cb2d329a98624
HowieHz <howie_hzgo@outlook.com>
2025-11-24 18:14:04 +0800

    Enable 'terser' minification in Vite build configs

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,7 @@ export default defineConfig({
     outDir: fileURLToPath(new URL("./templates/", import.meta.url)),
     assetsDir: "assets/dist/",
     emptyOutDir: true,
+    minify: "terser",
     rollupOptions: {
       input: {
         archives: path.resolve(__dirname, "src/templates/archives.html"),
