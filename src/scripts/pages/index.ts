4d85cf1d9e2af97882c6bb933c39827aea233358
HowieHz <howie_hzgo@outlook.com>
2025-11-24 14:41:00 +0800

    Import article mixin CSS in index.ts

--- src/scripts/pages/index.ts
+++ src/scripts/pages/index.ts
@@ -2,3 +2,4 @@ import "../generic-header";
 import "../../styles/pages/index.css";
 import "../../styles/mixins/common-post-list.css";
 import "../../styles/mixins/post-list-summary.css";
+import "../../styles/mixins/article.css"; // 瞬间列表
