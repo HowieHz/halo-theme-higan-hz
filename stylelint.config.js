e2d86f79b232d6e787605f90e8b9fca5d9bfd120
Howie Xie <94725606+HowieHz@users.noreply.github.com>
2026-01-27 11:22:52 +0800

    refactor: reset font-family stacks (#659)

--- stylelint.config.js
+++ stylelint.config.js
@@ -58,7 +58,18 @@ export default {
       },
     ],
 
+    // 关闭类选择器命名规则
     "selector-class-pattern": null,
+
+    // 值关键字大小写规则
+    "value-keyword-case": [
+      "lower", // 关键字要求小写
+      {
+        ignoreProperties: [
+          "/^--.*font-family.*/", // 忽略所有包含 font-family 的 CSS 变量
+        ],
+      },
+    ],
 
     // 其他规则
     "no-empty-source": null, // 允许空文件
