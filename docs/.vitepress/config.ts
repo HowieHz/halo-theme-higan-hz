2b2c4f96f10eeaf40f334963fc9323dd720bbeed
HowieHz <howie_hzgo@outlook.com>
2026-02-12 02:27:52 +0800

    chore: Exclude .html from VitePress include list

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -97,7 +97,7 @@ export default defineConfig({
           }),
         ],
         include: [
-          /\.(atom|rss|xml|xhtml|js|mjs|ts|html|json|css|eot|otf|ttf|svg|ico|bmp|dib|txt|text|log|md|conf|ini|cfg)$/,
+          /\.(atom|rss|xml|xhtml|js|mjs|ts|json|css|eot|otf|ttf|svg|ico|bmp|dib|txt|text|log|md|conf|ini|cfg)$/,
         ],
       }),
     ],
