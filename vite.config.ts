a9241ae82bb0d787097f0d12c45bb086489bdcd1
HowieHz <howie_hzgo@outlook.com>
2026-03-02 01:27:53 +0800

    chore: Increase build scheduler limit to 4

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
       ],
       // Set rate limiter to avoid build memory exceeding 4GB
       scheduler: {
-        limit: 3,
+        limit: 4,
         isHighMemory: (algo) => {
           if (algo === "zstandard") {
             return true;
