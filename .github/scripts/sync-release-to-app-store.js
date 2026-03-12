a39bbb16c3d2e5d4b57fd2da2fd1f357e6d8a2ce
HowieHz <94725606+HowieHz@users.noreply.github.com>
2026-03-12 17:12:52 +0000

    style: auto-format and fixes

--- .github/scripts/sync-release-to-app-store.js
+++ .github/scripts/sync-release-to-app-store.js
@@ -2,6 +2,7 @@ import { execFileSync } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
+
 import yaml from "js-yaml";
 
 const {
