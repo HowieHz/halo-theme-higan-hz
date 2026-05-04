fee92b03a7b96b9532b15662fe3536fb0bdca81b
HowieHz <howie_hzgo@outlook.com>
2026-05-04 16:33:06 +0800

    Use underscore as UTWM class prefix

--- vite.config.ts
+++ vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig((): UserConfig => {
       ? [
           utwm({
             generator: {
-              classPrefix: "-",
+              classPrefix: "_",
             },
           }),
         ]
