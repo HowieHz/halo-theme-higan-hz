063b6421f8a1dbedaa176702d3f325029739a051
HowieHz <howie_hzgo@outlook.com>
2026-05-19 15:24:43 +0800

    fmt

--- .github/scripts/release-version-policy.js
+++ .github/scripts/release-version-policy.js
@@ -1,6 +1,6 @@
 import { execFileSync } from "node:child_process";
-import { fileURLToPath } from "node:url";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const BUMP_LEVELS = {
   patch: 0,
