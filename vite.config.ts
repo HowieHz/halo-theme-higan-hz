d42835477393ef3f5d3cb22640872a56b6853c86
HowieHz <howie_hzgo@outlook.com>
2026-06-13 22:48:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -405,22 +405,6 @@ export default defineConfig((): UserConfig => {
       cssMinify: "lightningcss",
       rolldownOptions: {
         input,
-        output:
-          outputOption === "original"
-            ? undefined
-            : {
-                assetFileNames: (assetInfo) => {
-                  const isFontAsset = assetInfo.names.some((fileName) =>
-                    FONT_ASSET_EXTENSIONS.has(extname(fileName).toLowerCase()),
-                  );
-
-                  if (isFontAsset) {
-                    return "assets/[hash:7][extname]";
-                  }
-
-                  return `assets/[name]-[hash][extname]`;
-                },
-              },
       },
     },
   };
